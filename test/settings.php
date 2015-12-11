5420fa3116083bfa47c4755363fa622bc660e7c4
Søren Brønsted <sbr@ufds.dk>
2015-12-11 15:59:52 +0100

    Converted to autoload

--- test/settings.php
+++ test/settings.php
@@ -1,16 +1,3 @@
 <?php
 
-spl_autoload_register(function($class) {
-	$paths = array(
-		"types",
-	);
-
-	foreach($paths as $path) {
-		$fullname = $path.'/'.$class.'.php';
-		if (is_file($fullname)) {
-			include($fullname);
-			return true;
-		}
-	}
-	return false;
-});
+require 'vendor/autoload.php';
--- types/IllegalArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-class IllegalArgumentException extends ErrorException {
-  public function __construct($varName, $file, $line) {
-    parent::__construct("IllegalArgument for $varName ($file,$line)");
-  }
-}
-?>

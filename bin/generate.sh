bf39e69ac73625708744eb1c03cec670c29d27b0
Søren Brønsted <sbr@ufds.dk>
2016-03-23 14:19:38 +0100

    Ported to new build system

--- bin/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ $# -ne 1 ]
-then
-  echo "Wrong number of arguments"
-  echo "Usage $0 <name-of-migrations-class>"
-  exit 1
-fi
-php -f vendor/ruckusing/ruckusing-migrations/generate.php $1
